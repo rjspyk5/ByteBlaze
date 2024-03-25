1a56a7b599bd495a1e0ef59783aefc64b96aa27f
rjspyk5 <rjspyk5@gmail.com>
2024-03-25 15:14:19 +0600

    toggle theme for one time

--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export const Header = () => {
+  const [theme, setTheme] = useState("light");
+  const handleClick = (e) => {
+    if (e.target.checked === true) {
+      setTheme("sunset");
+    } else {
+      setTheme("light");
+    }
+  };
+  useEffect(() => {
+    document.getElementById("html").setAttribute("data-theme", theme);
+  }, [theme]);
+
   return (
     <div className="flex justify-between ">
-      <NavLink className="font-bold text-2xl" to="/">
-        ByteBlaze
+      <NavLink className="font-bold text-2xl text-primary" to="/">
+        <span className="text-[#FF00D3]">Byte</span>Blaze
       </NavLink>
-      <div className="space-x-6 *:font-bold">
+      <div className="space-x-6 flex items-center *:font-bold">
         <NavLink
           to="/"
           className={({ isActive }) => (isActive ? "text-primary" : "")}
@@ -26,6 +38,7 @@ export const Header = () => {
         >
           Bookmarks
         </NavLink>
+        <input onClick={handleClick} type="checkbox" className="toggle" />
       </div>
     </div>
   );
