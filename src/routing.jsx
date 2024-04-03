47fe087a217f83f0921cb67945258c1fab4de0d2
rjspyk5 <rjspyk5@gmail.com>
2024-04-03 23:59:41 +0600

    added blogs to routing

--- src/routing.jsx
+++ src/routing.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import { Root } from "../src/Root/Root";
 import { Home } from "./Pages/Home/Home";
 import { ErrorPage } from "./Components/ErrorPage/ErrorPage";
+import { Blogs } from "./Pages/Blogs/Blogs";
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/blogs",
+        element: <Blogs />,
       },
     ],
   },
